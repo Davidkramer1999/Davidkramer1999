ac748132267a1f755e2a29d42a201b56106ee6bc
kramer <david.kramer@gmail.com>
2023-09-14 19:57:51 +0200

    adding animations

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -1,11 +1,25 @@
+import { motion, useAnimation } from 'framer-motion';
+
 const Card = ({ title, content, isActive, onClick }) => {
+  const controls = useAnimation();
+  
+  const hoverWaveEffect = async () => {
+    await controls.start({
+      scaleX: [1, 1.05, 1, 1.05, 1],
+      scaleY: [1, 0.95, 1, 0.95, 1],
+    });
+  };
+
   return (
-    <div 
+    <motion.div 
+      animate={controls}
       onClick={onClick}
+      onHoverStart={hoverWaveEffect}
       className={`transform duration-500 ${isActive ? 'scale-110 z-10' : 'scale-100 z-0'} flex flex-col justify-center items-center p-8 rounded-lg ${isActive ? 'bg-blue-100 text-gray-700' : 'bg-blue-200 text-gray-700'} transition-all ease-in-out shadow-lg border-4 border-gray-500`}
     >
-      <h1 className="text-lg font-bold whitespace-normal break-words">{title}</h1>
-      {isActive && <p className="leading-relaxed whitespace-normal break-words max-w-xs">{content}</p>}</div>
+      <h1 className="text-lg font-bold">{title}</h1>
+      {isActive && <p className="leading-relaxed">{content}</p>}
+    </motion.div>
   );
 };
 
